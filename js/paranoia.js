74cf598ab5e7f29e094e41b45423359cb8791483
Ethan Gruening <ethgru@iastate.edu>
2024-03-05 15:24:20 -0600

    Now paranoia is done.

--- js/paranoia.js
+++ js/paranoia.js
@@ -30,6 +30,51 @@ document.addEventListener("DOMContentLoaded", function() {
         })
         .catch(error => console.error('Error fetching JSON:', error));
 });
+function toggleQuestionVisibility() {
+    const questionText = document.getElementById('questionText');
+    const hideButton = document.getElementById('hideButton');
+    const currentVisibility = questionText.style.visibility;
+
+    if (currentVisibility === 'hidden' || currentVisibility === '') {
+    questionText.style.visibility = 'visible';
+    hideButton.textContent = 'Hide Question'
+
+    } else {
+    questionText.style.visibility = 'hidden';
+    hideButton.textContent = 'Show Question'
+    }
+}
+
+function flipCoin() {
+    const coinImage = document.getElementById('coinImage');
+    const randomValue = Math.floor(Math.random() * 10001);
+    if (randomValue % 2 == 1) {
+        coinImage.src = '../images/heads.png';
+    } else {
+        coinImage.src = '../images/tails.png';
+    }
+
+    coinImage.style.visibility = 'visible';
+}
+
+function changeQuestionText() {
+
+    const questionText = document.getElementById('questionText');
+
+    fetch("../json/paranoia.json")
+    .then(response => response.json())
+    .then(json => setResult(json, questionText));
+
+    function setResult(json, questionText) {
+        questionText.textContent = json.questions[Math.floor(Math.random()*json.questions.length)];
+    }
+
+    const hideButton = document.getElementById('hideButton');
+    const currentVisibility = questionText.style.visibility;
+
+    questionText.style.visibility = 'visible';
+    hideButton.textContent = 'Hide Question'
+}
 
 function toggleAbout() {
     var aboutDropdown = document.getElementById("aboutDropdown");
