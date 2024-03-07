738dd121c6fe229242ba7471910f79d967e08be2
Isaac Denning <isaac11182003@gmail.com>
2024-03-06 20:08:25 -0600

    Changed hamburger to add other content

--- js/main.js
+++ js/main.js
@@ -8,20 +8,38 @@ document.addEventListener("DOMContentLoaded", function() {
 
             jsonData.imageButtons.forEach(contentObject => {
 
-                const imageContainer = document.createElement("div");
-                imageContainer.className = "image-container";
+                const container = document.createElement("div");
+                container.className = "image-container";
+                container.width = 150;
 
-                const imageLink = document.createElement("a");
-                imageLink.href = contentObject.html_filepath;
+                const gameHeader = document.createElement("b");
+                gameHeader.textContent = contentObject.game_name;
+
+                const gameDescription = document.createElement("small");
+                gameDescription.textContent = contentObject.game_description;
+
+                const creatorName = document.createElement("i");
+                creatorName.textContent = contentObject.game_creator;
+                const creatorTag = document.createElement("b")
+                creatorTag.textContent = "Creator: ";
+                const gameCreator = document.createElement("small");
+                gameCreator.appendChild(creatorTag);
+                gameCreator.appendChild(creatorName);
 
                 const contentImage = document.createElement("img");
                 contentImage.src = contentObject.image_filepath;
                 contentImage.alt = contentObject.game_name;
                 contentImage.width = 150;
-
+                const imageLink = document.createElement("a");
+                imageLink.href = contentObject.html_filepath;
                 imageLink.appendChild(contentImage);
-                imageContainer.appendChild(imageLink);
-                hamburgerDropdown.appendChild(imageContainer);
+
+                container.appendChild(gameHeader);
+                container.appendChild(imageLink);
+                container.appendChild(gameDescription);
+                container.appendChild(document.createElement("br"));
+                container.appendChild(gameCreator);
+                hamburgerDropdown.appendChild(container);
             });
         })
         .catch(error => console.error('Error fetching JSON:', error));
